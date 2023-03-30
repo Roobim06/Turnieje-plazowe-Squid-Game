15c5ac27eed2832027b2e2299effdea0018edf55
null <null>
2023-03-30 08:39:54 +0200

    ok

--- Projekt/ConsoleApp1/Program.cs
+++ Projekt/ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ public static class Program
 {
     public static void Main()
     {
-        Console.WriteLine("hello world");
+        Osoba osoba = new Osoba();
+
+
     }
 }
--- Projekt/ConsoleApp1/klasa Osoby.cs
+++ Projekt/ConsoleApp1/klasa Osoby.cs
@@ -1,4 +1,55 @@
-public class Osoby
+
+public class Osoba
 {
     string imie, nazwisko;
+    public Osoba()
+    {
+        Console.Write("Podaj imie: ");
+        string imie = Console.ReadLine();
+        Console.Write("Podaj nazwisko: ");
+        string nazwisko = Console.ReadLine();
+    }
+    public void Edytuj()
+    {
+        bool warunek = true;
+        while (warunek)
+        {
+            Console.WriteLine("Co chcesz edytowac?\n1 - Imie\n2 - Nazwisko");
+            switch (Console.ReadLine())
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    warunek = false;
+                    break;
+            }
+        }
+
+    }
 }
+public class Uczestnik : Osoba
+{
+    string pseudonim;
+    public Organizator()
+    {
+
+    }
+}
+public class Sedzia : Osoba
+{
+    public Sedzia()
+    {
+        
+    }
+}
+public class Organizator : Osoba
+{
+    int numer_telefonu;
+    public Organizator()
+    {
+
+    }
+}
+
