11e37eaa205001d013e0d84afe2cef35e16b9b3c
null <null>
2023-03-30 08:12:25 +0200

    klasa dyscypliny zostala zrobiona

---
+++ Projekt/ConsoleApp1/klasa Osoby.cs
@@ -0,0 +1,4 @@
+public class Osoby
+{
+    string imie, nazwisko;
+}
---
+++ Projekt/ConsoleApp1/klasa_Dyscypliny.cs
@@ -0,0 +1,42 @@
+﻿public class Dyscypliny
+{
+	string nazwa;
+	string zasady;
+	int czas_trwania;
+	public Dyscypliny(string nazwa, string zasady, int czas_trwania)
+	{
+		this.nazwa = nazwa;
+		this.zasady = zasady;
+		this.czas_trwania = czas_trwania;
+		Console.WriteLine("Dyscyplina zostala stworzona!");
+	}
+	public void EdytujDyscypline()
+	{
+		Console.Write("Co chcesz zmienic 1-nazwe 2-zasady 3-czas trwania (w min): ");
+		int x = Convert.toInt32(Console.ReadLine());
+		switch (x)
+		{
+			case 1:
+				Console.Write("Podaj nowa nazwe: ");
+				string y = Console.ReadLine();
+				this.nazwa = y;
+				break;
+            case 2:
+                Console.Write("Podaj nowe zasady: ");
+                string y = Console.ReadLine();
+                this.zasady = y;
+                break;
+            case 2:
+                Console.Write("Podaj nowy czas trwania (w min): ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                this.czas_trwania = y;
+                break;
+        }
+	}
+	public void WypiszDyscypline()
+	{
+		Console.WriteLine("Dyscyplina: {0}", this.nazwa);
+        Console.WriteLine("Zasady: {0}", this.zasady);
+        Console.WriteLine("Czas trwania w minutach: {0}", this.czas_trwania);
+    }
+}
