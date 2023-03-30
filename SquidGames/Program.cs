6f3d8e9b56c652ba89dae73870f8be031ae9e757
klocmaster69 <129272531+klocmaster69@users.noreply.github.com>
2023-03-30 09:20:27 +0200

    od nowa zrobilem

---
+++ SquidGames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public static class Program
+{
+    public static void Main()
+    {
+        Dyscypliny dyscypliny = new Dyscypliny("aaaa","bbbb",123);
+        dyscypliny.WypiszDyscypline();
+        dyscypliny.EdytujDyscypline();
+        dyscypliny.WypiszDyscypline();
+
+    }
+    public static void PodajString(string content)
+    {
+        Console.Write($"Podaj {content}: ");
+        string nazwisko = Console.ReadLine();
+    }
+}
